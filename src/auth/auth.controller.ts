da631c6a4e016488d3907a9db4719389ffd6ee35
Carlos Feliz <carlosjfeliz7@gmail.com>
2023-09-04 22:11:18 -0400

    e2e test added for Auth module and partial User module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,18 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { SignInDto, SignUpDto } from "./dto";
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto, SignUpDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-    
-    constructor(private authService:AuthService) {
+  constructor(private authService: AuthService) {}
+  @HttpCode(200)
+  @Post('signin')
+  signin(@Body() dto: SignInDto) {
+    return this.authService.signin(dto);
+  }
 
-    }
-
-    @Post('signin')
-    signin(@Body() dto: SignInDto){
-        return this.authService.signin(dto);        
-    }
-
-    @Post('signup')
-    signup(@Body() dto: SignUpDto) {
-        return this.authService.signup(dto)
-    }
-
-    @Get('testing')
-    testing() {
-        return 'This is a test'
-    }
+  @Post('signup')
+  signup(@Body() dto: SignUpDto) {
+    return this.authService.signup(dto);
+  }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,11 +3,27 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
+import { SignInDto, SignUpDto } from 'src/auth/dto';
+
+const mockData = {
+  signIn: {
+    email: 'test@mail.com',
+    password: '123456789',
+  },
+  signUp: {
+    email: 'test@mail.com',
+    password: '123456789',
+    firstName: 'John',
+    lastName: 'Doe',
+  },
+};
+
+let app: INestApplication;
+let prisma: PrismaService;
+let signInData: SignInDto;
+let signUpData: SignUpDto;
 
 describe('App e2e', () => {
-  let app: INestApplication;
-  let prisma: PrismaService;
-
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -32,7 +48,122 @@ describe('App e2e', () => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    beforeAll(() => {
+      signUpData = mockData.signUp;
+    });
+    describe('Signup', () => {
+      it('Creates a user', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(signUpData)
+          .expectStatus(201)
+          .expectBodyContains('access_token');
+      });
 
-  // describe('Auth', () => {});
+      it('Thows error if no email', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            ...signUpData,
+            email: '',
+          })
+          .expectStatus(400);
+      });
+
+      it('Thows error if no password', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            ...signUpData,
+            password: '',
+          })
+          .expectStatus(400);
+      });
+
+      it('Thows error if password less than 8 charts', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            ...signUpData,
+            password: '123',
+          })
+          .expectStatus(400);
+      });
+
+      it('Thows error no Body passed', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+    });
+    describe('Signin', () => {
+      beforeEach(() => {
+        signInData = mockData.signIn;
+      });
+
+      it('Thows an error is no Body passed', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400);
+      });
+
+      it('Should returns jwt access token if login successes', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(signInData)
+          .expectStatus(200)
+          .expectBodyContains('access_token')
+          .stores((resquest, response) => {
+            return {
+              userAt: response.body.access_token,
+            };
+          });
+      });
+
+      it('Thows an error is no email passed', () => {
+        signInData.password = '';
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(signInData)
+          .expectStatus(400);
+      });
+
+      it('Thows an error is no password passed', () => {
+        signInData.email = '';
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(signInData)
+          .expectStatus(400);
+      });
+    });
+  });
+
+  describe('Users', () => {
+    describe('Get me', () => {
+      it('Should return a user with a given access_token', () => {
+        const customMock = { ...mockData.signUp };
+        delete customMock.password;
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200)
+          .expectJsonLike(customMock);
+      });
+    });
+    describe('Edit user', () => {});
+  });
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+    describe('Get bookmarks', () => {});
+    describe('Get bookmark by ID', () => {});
+    describe('Update bookmark', () => {});
+    describe('Delete bookmark', () => {});
+  });
 });
